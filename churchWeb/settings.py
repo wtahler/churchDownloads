cd91c22a67d82346d957ef4c8832241d9dbbc936
Bill Tahler <wtahler@gmail.com>
2012-01-27 01:18:49 -0500

    ugh

--- churchWeb/settings.py
+++ churchWeb/settings.py
@@ -1,5 +1,6 @@
 # Django settings for churchWeb project.
-
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -46,7 +47,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/bill/heroku/churchWeb/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'templates')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -109,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/bill/heroku/churchWeb/templates/"
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
